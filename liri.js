82089faea9d1c1ff3d5f9f343ef59deb6b7664ee
Ryan Isaacson <munchinator6354@gmail.com>
2019-11-10 20:24:52 -0800

    added forloop to cycle through multiple concert-this band options, still limited to 1 word bands

--- liri.js
+++ liri.js
@@ -28,12 +28,16 @@ function bandsInTownAPI(input) {
         get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
         .then(function (response) {
             // console.log(response.data);
-            console.log(
-                "Band: " + input + "\n" +
-                "Venue: " + response.data[0].venue.name + "\n" +
-                "Location: " + response.data[0].venue.city + ", " + response.data[0].venue.country + "\n" +
-                "Date:" +  response.data[0].datetime
-            );
+            for (var i = 0; i < response.data.length; i++) {
+                console.log(
+                    "-----------------------------------------------------------" + "\n" +
+                    "Band: " + input + "\n" +
+                    "Venue: " + response.data[i].venue.name + "\n" +
+                    "Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country + "\n" +
+                    "Date:" + response.data[i].datetime + "\n"
+                );
+            }
+
         })
         .catch(function (error) {
             if (error.response) {
